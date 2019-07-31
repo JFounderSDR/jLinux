f06c4025c518ee037ad3cdea05315cfbcc7c13bf
JF-YHL <13820023307@163.com>
2019-07-31 11:08:50 +0800

    add ipv6 support and jf-spi support

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 8 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#8 SMP PREEMPT Tue Jul 30 20:14:13 CST 2019"
+#define LINUX_COMPILE_BY "yhl"
+#define LINUX_COMPILE_HOST "yhl-OptiPlex-3060"
+#define LINUX_COMPILER "gcc version 6.5.0 (Linaro GCC 6.5-2018.12) "
